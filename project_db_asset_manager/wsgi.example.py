2126a8b009728874e825f13f2ea3fc54568a6a00
hasibomi <hrahmanomi@gmail.com>
2020-09-09 02:10:40 +0600

    wsgi example added

--- project_db_asset_manager/wsgi.example.py
+++ project_db_asset_manager/wsgi.example.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/var/www/html/db-asset-manager.cimplux.com')
+# adjust the Python version in the line below as needed
+sys.path.append('/var/www/html/db-asset-manager.cimplux.com/env/lib/python3.6/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_db_asset_manager.settings')
 
