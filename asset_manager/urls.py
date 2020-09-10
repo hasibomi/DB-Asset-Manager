4599b00e7da91ea26d39970fdc9be3edf8c4a528
hasibomi <hrahmanomi@gmail.com>
2020-09-11 01:59:53 +0600

    Code refactored to support Detail view & extend  features

--- asset_manager/urls.py
+++ asset_manager/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = 'asset_manager'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('connect/<id>/', ConnectToAConnectionView.as_view(), name='connect'),
+    path('connect/<pk>/', ConnectToAConnectionView.as_view(), name='connect'),
     path('<pk>/delete/', DeleteAConnection.as_view(), name='delete'),
 ]
--- asset_manager/views.py
+++ asset_manager/views.py
@@ -1,12 +1,25 @@
 import os, shutil
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    get_object_or_404
+)
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, FormView, DeleteView
+from django.views.generic import (
+    CreateView,
+    FormView,
+    DetailView,
+    DeleteView
+)
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 from mysql.connector import connect
 from .models import Connection
-from .forms import NewConnectionForm, ColumnToDIrectoryCompareForm
+from .forms import (
+    NewConnectionForm,
+    ColumnToDIrectoryCompareForm
+)
 
 
 class IndexView(SuccessMessageMixin, CreateView):
@@ -22,19 +35,13 @@ class IndexView(SuccessMessageMixin, CreateView):
         return context
 
 
-class ConnectToAConnectionView(SuccessMessageMixin, FormView):
+class ConnectToAConnectionView(SuccessMessageMixin, DetailView):
     directory = None
     backup_directory = None
     restore_directory = None
     template_name = 'asset_manager/connect.html'
-    form_class = ColumnToDIrectoryCompareForm
-    success_message = 'Files are cleared'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['connection'] = get_object_or_404(Connection, id=self.kwargs['id'])
-
-        return context
+    model = Connection
+    context_object_name = 'connection'
 
     def remove_trailing(self, value, string):
         if value.endswith(string):
@@ -42,7 +49,44 @@ class ConnectToAConnectionView(SuccessMessageMixin, FormView):
 
         return value
 
-    def form_valid(self, form):
+    def backup_files(self, query_result):
+        for row in query_result:
+            if row[0]:
+                file = self.directory + '/' + row[0]
+                dest = self.backup_directory + '/' + row[0].split('/')[-1]
+
+                if os.path.exists(file):
+                    shutil.copy(file, dest)
+
+    def delete_all_files(self, src):
+        for file in os.listdir(src):
+            os.remove(os.path.join(src, file))
+
+    def restore_files(self, src):
+        for file in os.listdir(src):
+            from_file = os.path.join(src, file)
+            to = os.path.join(self.restore_directory, file)
+            shutil.copy(from_file, to)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ColumnToDIrectoryCompareForm()
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = ColumnToDIrectoryCompareForm(request.POST)
+
+        if not form.is_valid():
+            return render(
+                request,
+                self.template_name,
+                context={
+                    'form': form,
+                    'connection': self.get_object()
+                }
+            )
+            
         self.directory = self.remove_trailing(form.cleaned_data['directory'], '/')
         self.backup_directory = self.remove_trailing(form.cleaned_data['backup_directory'], '/')
         self.restore_directory = self.remove_trailing(form.cleaned_data['restore_directory'], '/')
@@ -53,7 +97,7 @@ class ConnectToAConnectionView(SuccessMessageMixin, FormView):
 
         if not os.path.exists(self.directory):
             messages.error(
-                self.request,
+                request,
                 '{} does not exist'.format(self.directory)
             )
 
@@ -65,19 +109,17 @@ class ConnectToAConnectionView(SuccessMessageMixin, FormView):
         if not os.path.exists(self.restore_directory):
             os.makedirs(self.restore_directory)
 
-        connection = get_object_or_404(Connection, id=self.kwargs['id'])
-
         try:
             con = connect(
-                host=connection.db_host,
-                port=connection.db_port,
-                user=connection.db_user,
-                password=connection.db_pass,
+                host=self.get_object().db_host,
+                port=self.get_object().db_port,
+                user=self.get_object().db_user,
+                password=self.get_object().db_pass,
                 database=db_name
             )
         except Exception as e:
-            messages.error(self.request, e)
-            return redirect('asset_manager:connect', id=self.kwargs['id'])
+            messages.error(request, e)
+            return redirect('asset_manager:connect', pk=self.kwargs['pk'])
 
         cursor = con.cursor()
 
@@ -98,31 +140,8 @@ class ConnectToAConnectionView(SuccessMessageMixin, FormView):
         if form.cleaned_data['delete_backup_directory']:
             shutil.rmtree(self.backup_directory)
 
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse('asset_manager:connect', kwargs={
-            'id': self.kwargs['id']
-        })
-
-    def backup_files(self, query_result):
-        for row in query_result:
-            if row[0]:
-                file = self.directory + '/' + row[0]
-                dest = self.backup_directory + '/' + row[0].split('/')[-1]
-
-                if os.path.exists(file):
-                    shutil.copy(file, dest)
-
-    def delete_all_files(self, src):
-        for file in os.listdir(src):
-            os.remove(os.path.join(src, file))
-
-    def restore_files(self, src):
-        for file in os.listdir(src):
-            from_file = os.path.join(src, file)
-            to = os.path.join(self.restore_directory, file)
-            shutil.copy(from_file, to)
+        messages.success(request, 'Files are cleared')
+        return redirect(request, 'asset_manager:connect', pk=self.kwargs['pk'])
 
 
 class DeleteAConnection(SuccessMessageMixin, DeleteView):
